4b6ba9c32fa9828600bb02ab72f3dd2ac4323b2a
Ethan Tatum <Ethan.Tatum@edgenuity.com>
2019-10-18 13:26:52 -0700

    completed zombie game

--- 10-Node/10-01-29/Unsolved/zombiegame.js
+++ 10-Node/10-01-29/Unsolved/zombiegame.js
@@ -14,18 +14,88 @@
 // Major Warning: inquirer's prompt function is "asynchronous", which means that the majority of your game logic will need to be inside the .then() function for your prompt. 
 
 // ===========================================================================================================
-let inquirer = require("inquirer");
+const inquirer = require("inquirer");
+const colors = require("colors");
 
-inquirer
-    .prompt([
-        {
-            type: "checkbox",
-            name: "userGuess",
-            message: "Select a number between 1 and 5.",
-            choices: [1, 2, 3, 4, 5]
-        }
-    ])
-    .then(userGuess => {
-        console.log(userGuess);
-        
-    });
+console.log(`
+    +*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+
+    *+                             +*
+    +*  Welcome to Zombie Slayer!  *+
+    *+                             +*
+    +*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+
+`.trap.bold);
+
+let userHealth = 40;
+let zombieHealth = 10;
+let choices = ["Tire Iron", "Pistol", "Sledgehammer", "Pitchfork", "Decorative Samurai Sword"];
+
+function gamePlay() {
+    console.log(`
+Your health is ${userHealth}.`.bgGreen.black.bold);
+    console.log(`
+The zombie's health is ${zombieHealth}.`.bgRed.black);
+
+
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                name: "userWeapon",
+                message: "Choose your weapon - but choose carefully!",
+                choices: choices
+            }
+        ])
+        .then(response => {
+            console.log(`
+You choose a ${response.userWeapon}.
+            `);
+            let randomWeapon = choices[Math.floor(Math.random() * choices.length)];
+            let hitPoints = Math.floor(Math.random() * 5) + 1;
+            if (response.userWeapon === randomWeapon) {
+                console.log(`Your ${response.userWeapon} strikes a blow! The zombie loses ${hitPoints} health!
+                `.bold.green);
+                zombieHealth = zombieHealth - hitPoints;
+                if (zombieHealth <= 0) {
+                    return console.log(`A killing blow! The zombie shudders and lies still - you are victorious!
+                    `.brightGreen);
+                } else {
+                    playAgain();
+                }
+            } else {
+                console.log(`Oh no! The zombie dodged your ${response.userWeapon} attack, and manages to bite you.
+You lose ${hitPoints} health!
+                `.bold.red);
+                userHealth = userHealth - hitPoints;
+                if (userHealth <= 0) {
+                    return console.log(`That final bite takes the last of your strength, and as your eyes slowly close, 
+your only thought is "Please don't let me become one of them..."
+                    `.bold.brightRed);
+                } else {
+                    playAgain();
+                }
+            }
+
+        });
+};
+
+function playAgain() {
+    inquirer
+        .prompt([
+            {
+                type: "confirm",
+                name: "playAgain",
+                message: "Will you try to attack again?"
+            }
+        ])
+        .then(response => {
+            if (response.playAgain) {
+                gamePlay();
+            } else {
+                console.log(`You try to escape, but the zombie pounces...
+you hear yourself scream as all fades to black.
+                `.bold.brightRed);
+            }
+        })
+};
+
+gamePlay();
