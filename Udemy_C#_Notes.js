44448f2d62e53960bd237616ebef0287eaf86011
Ethan Tatum <Ethan.Tatum@edgenuity.com>
2019-10-22 11:53:03 -0700

    added C# notes

---
+++ Udemy_C#_Notes.js
@@ -0,0 +1,15 @@
+//  Solutions can have multiple projects inside
+//  Adding Console.ReadLine() will leave the window open (but not advised)
+//  Building will compile the code and identify any problems
+//  Can move around windows and views in VS
+//  int x = 1;
+//  char letter = 'a';
+//  string name = "Jon";
+//  int: used to store integer (whole) numbers
+//  float: used to store floating point numbers (add 'f' at end of number)
+// double:  used to store floating point numbers (add 'd' at end of number)
+//  decimal: used to store money values (most specific) (add 'm' at end of number)
+//  string: used to store text
+//  bool: used to store true/false
+//  Can declare multiple variables with single data type (ie "string firstName lastName")
+//  Console.WriteLine inserts new lines, Console.Write does not
